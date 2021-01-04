8c7b1c81ff3e2e18d70b961e5ba3dc23c696c48f
Nathan Rice <nrice778@gmail.com>
2021-01-04 15:46:18 -0600

    init repo

---
+++ Testing/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<ProductModel> GetAllProducts();
+    }
+}
---
+++ Testing/ProductRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<ProductModel> GetAllProducts()
+        {
+            return _conn.Query<ProductModel>("SELECT * FROM products;");
+        }
+    }
+}
